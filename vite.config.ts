ce234261cfea781ad3e043f324ca3dd98213a7f5
yourdudeness <30469345+yourdudeness@users.noreply.github.com>
2025-02-27 11:28:21 +0100

    set up

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import autoprefixer from 'autoprefixer'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),tailwindcss(),],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({})
+      ],
+    }
+  }
+})
