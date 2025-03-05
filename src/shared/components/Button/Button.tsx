c42022237eddcf4fece415014b9902d1789dd1f9
yourdudeness <30469345+yourdudeness@users.noreply.github.com>
2025-03-05 23:32:43 +0100

    fix(button): fix interface for button

--- src/shared/components/Button/Button.tsx
+++ src/shared/components/Button/Button.tsx
@@ -2,11 +2,16 @@ import clsx from "clsx";
 
 import styles from "./button.module.css";
 
-interface ButtonProps {
+interface ButtonPrimitiveProps
+  extends React.ComponentPropsWithoutRef<"button"> {
   children?: React.ReactNode;
   intent?: "neutral" | "accent";
 }
-export const Button = ({ children, intent = "neutral" }: ButtonProps) => {
+
+export const Button = ({
+  children,
+  intent = "neutral"
+}: ButtonPrimitiveProps) => {
   return (
     <button className={clsx(styles.root)} data-intent={intent}>
       {children}
