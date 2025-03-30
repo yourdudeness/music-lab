755d41bd1341ff83bd20673330d92bb02507bb9f
yourdudeness <30469345+yourdudeness@users.noreply.github.com>
2025-03-30 14:34:43 +0200

    fix: trying fix problem with url

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,10 +74,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   port: process.env.PORT ? Number(process.env.PORT) : undefined,
-  //   reuseExistingServer: !process.env.CI,
-  // }
+  webServer: {
+    command: "npm run test:integration",
+    url: "http://localhost:5173",
+    port: process.env.PORT ? Number(process.env.PORT) : undefined,
+    reuseExistingServer: !process.env.CI
+  }
 });
