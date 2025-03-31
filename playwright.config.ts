1b8ad07fec693bb8874d1efdf43ccf5968390aff
yourdudeness <30469345+yourdudeness@users.noreply.github.com>
2025-03-31 18:11:00 +0200

    feat: add server for playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,10 +74,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   port: process.env.PORT ? Number(process.env.PORT) : undefined,
-  //   reuseExistingServer: !process.env.CI,
-  // }
+  webServer: {
+    command: "npm run dev",
+    port: process.env.PORT ? Number(process.env.PORT) : undefined,
+    reuseExistingServer: !process.env.CI
+  }
 });
